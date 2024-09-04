28b63e150b859ab925fcd70afbd30a29a9a88f1f
rizkypratamayudha <masrizki87@gmail.com>
2024-09-04 09:08:26 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/haloo', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'Rizky Pratama Yudha NIM : 2241760020';
+});
+
+//route parameters
+Route::get('/user/{RizkyPratamaYudha}', function ($name){
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}',function ($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id){
+    return 'Halaman Artikel : '.$id;
+});
+
+//optional parameters
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya '.$name;
+});
